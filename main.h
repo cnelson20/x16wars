1bba8333318b3932beff32de0b08d79ebdeb94f3
cnelson20 <cjnelson2004@gmail.com>
2022-09-09 21:15:27 -0400

    fix ?

--- main.h
+++ main.h
@@ -18,16 +18,3 @@ void clearUI();
 
 void keyPressed();
 void clearScreen();
-
-#define OPTION_NULL 0
-#define OPTION_END 1
-#define OPTION_CONCEDE 2
-#define OPTION_QUIT 3 
-#define OPTION_DROP 4
-#define OPTION_WAIT 5 
-#define OPTION_LOAD 6
-#define OPTION_CAPTURE 7
-#define OPTION_ATTACK 8
-#define OPTION_MOUSETOGGLE 9
-#define OPTION_JOIN 10
-#define OPTION_MENU 11
